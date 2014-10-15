5dacaeae7988c9de93efbb2c5e3d1ce90e0d4303
Wenda Xiao <wenda@ua-unistudent-ten-9-179-162.uniaccess.unimelb.edu.au>
2014-10-15 21:55:45 +1100

    add boostrap

--- db/schema.rb
+++ db/schema.rb
@@ -31,17 +31,8 @@ ActiveRecord::Schema.define(version: 20141014103040) do
     t.integer "effect_id", null: false
   end
 
-  create_table "filters", force: true do |t|
-    t.float    "hue"
-    t.float    "saturation"
-    t.float    "contrast"
-    t.float    "brightness"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "galleries", force: true do |t|
-    t.string   "name"
+    t.integer  "number_of_images"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
@@ -58,35 +49,21 @@ ActiveRecord::Schema.define(version: 20141014103040) do
   end
 
   create_table "images", force: true do |t|
-    t.float    "height"
+    t.float    "length"
     t.float    "width"
     t.string   "title"
     t.text     "caption"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "owner"
     t.float    "size"
-    t.string   "image"
+    t.string   "url"
     t.integer  "user_id"
   end
 
   create_table "images_users", id: false, force: true do |t|
     t.integer "user_id",  null: false
     t.integer "image_id", null: false
-  end
-
-  create_table "media", force: true do |t|
-    t.string   "owner"
-    t.float    "size"
-    t.string   "type"
-    t.string   "url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "media_id"
-  end
-
-  create_table "media_users", id: false, force: true do |t|
-    t.integer "user_id",   null: false
-    t.integer "medium_id", null: false
   end
 
   create_table "users", force: true do |t|
